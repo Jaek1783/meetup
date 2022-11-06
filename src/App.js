da49db006cbc4bb7f41989055ca3b0f781e89b5e
jaekeun Choi <jaek1783@gmail.com>
2022-11-06 21:06:38 +0900

    라우터 네비게이션 추가하기

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import {Routes, Route} from 'react-router-dom';
 import AllMeetupsPages from './pages/AllMeetup';
 import FavoritesPages from './pages/Favorites';
 import NewMeetupPages from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
     <Routes>
       <Route path='/' element={<AllMeetupsPages/>}/>
       <Route path='/favorites' element={<FavoritesPages/>}/>
