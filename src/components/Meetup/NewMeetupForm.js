8194651ea697938242bb5e4ac95b0e3c4db4e853
jaekeun Choi <jaek1783@gmail.com>
2022-11-09 11:07:25 +0900

    onSubmit 함수 sumitHandler() 생성/addMeetup버튼 클릭시 meetupData 저장

--- src/components/Meetup/NewMeetupForm.js
+++ src/components/Meetup/NewMeetupForm.js
@@ -1,23 +1,45 @@
 import Card from "../UI/Card";
 import modules from './NewMeetupForm.module.css';
+import {useRef} from 'react';
+
 const NewMeetupForm = ()=>{
+    const titleRef = useRef(null);
+    const imageRef = useRef(null);
+    const addressRef = useRef(null);
+    const descRef = useRef(null);
+
+    const submitHandler = (event)=>{
+        event.preventDefault();
+    const enterTitle = titleRef.current.value;
+    const enterImage = imageRef.current.value;
+    const enterAddress = addressRef.current.value;
+    const enterDesc = descRef.current.value;
+
+    const meetupData = {
+        title:enterTitle,
+        image:enterImage,
+        address:enterAddress,
+        description:enterDesc
+    };
+    console.log(meetupData);
+    };
     return <Card>
-        <form className={modules.form}>
+        <form className={modules.form} onSubmit={submitHandler}>
             <div className={modules.control}>
                 <label htmlFor="title">Meetup Title</label>
-                <input type="text" required id='title'/>
+                <input type="text" required id='title' ref={titleRef}/>
             </div>
             <div className={modules.control}>
                 <label htmlFor="image">Meetup Image</label>
-                <input type="url" required id='image'/>
+                <input type="url" required id='image' ref={imageRef}/>
             </div>
             <div className={modules.control}>
                 <label htmlFor="address">Meetup address</label>
-                <input type="text" required id='address'/>
+                <input type="text" required id='address'ref={addressRef}/>
             </div>
             <div className={modules.control}>
                 <label htmlFor="description">Description</label>
-                <textarea id='description' required rows='5'></textarea>
+                <textarea id='description' required rows='5' ref={descRef}></textarea>
             </div>
             <div className={modules.actions}>
                 <button>Add Meetup</button>
