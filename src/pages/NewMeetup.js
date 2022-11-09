8d720469dfb4715f4cdd4fd1d4b4330e9866f000
jaekeun Choi <jaek1783@gmail.com>
2022-11-09 13:07:29 +0900

    axios 오류수정

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,11 +1,9 @@
 import NewMeetupForm from "../components/Meetup/NewMeetupForm";
 import axios from "axios";
 const NewMeetupPage = ()=>{
+    
     const addMeetupHandler = (meetupData)=>{
-    axios.post('https://react-serverless-4b643-default-rtdb.firebaseio.com/meetups.json',{meetupData})
-    .then(response=>{
-        console.log(response.data);
-    })};
+    axios.post('https://react-serverless-4b643-default-rtdb.firebaseio.com/meetups.json',meetupData)};
     return <div>
         <h2>Add New Meetup</h2>
         <NewMeetupForm onAddMeetup={addMeetupHandler}/>
