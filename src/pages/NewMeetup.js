4577ac3ff4522e49b4952e68a8336ba5b73b9b94
jaekeun Choi <jaek1783@gmail.com>
2022-11-09 14:13:48 +0900

    Revert "firebase에 data전송 후 navigate(/)되도록 수정"
    This reverts commit 1f13eb330e58be58bf8a76194d66012633deae8e.

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,16 +1,9 @@
 import NewMeetupForm from "../components/Meetup/NewMeetupForm";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 const NewMeetupPage = ()=>{
-    const navigate = useNavigate();
+    
     const addMeetupHandler = (meetupData)=>{
-    axios.post('https://react-serverless-4b643-default-rtdb.firebaseio.com/meetups.json',meetupData)
-    .then(response=>{
-        if(response.data){
-            navigate('/');
-        }
-    })
-    };
+    axios.post('https://react-serverless-4b643-default-rtdb.firebaseio.com/meetups.json',meetupData)};
     return <div>
         <h2>Add New Meetup</h2>
         <NewMeetupForm onAddMeetup={addMeetupHandler}/>
