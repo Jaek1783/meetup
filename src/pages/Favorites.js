ba8a406f3aa5073e2f3dd1480cb8b9e39a7414e3
jaekeun Choi <jaek1783@gmail.com>
2022-11-09 17:02:40 +0900

    favorite 컴포넌트와 contextAPI 연결/add 되어 있는 meetupItem이 추가됨

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,17 @@
-const Favorites = ()=>{
-    return <div> Favorites pages</div>
+import { useContext } from "react";
+import FavoritesContext from "../store/favorites-context";
+import MeetupList from "../components/Meetup/MeetupList";
+const FavoritesPages = ()=>{
+    const favoritesCtx = useContext(FavoritesContext);
+    let content = null;
+    if(favoritesCtx.totalFavorites === 0){
+        content = <p>You got no Favorite yet. Start adding some?</p>
+    }else{
+        content = <MeetupList meetups={favoritesCtx.favorite}/>
+    }
+    return <div>
+        <h2>My Favorite</h2>
+        {content}
+    </div>
 };
-export default Favorites;
+export default FavoritesPages;
